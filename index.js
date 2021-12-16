cbb715081735e829ee1766c5be074dc0bed1c090
Roshnie Pillay <roshnie.pillayx@gmail.com>
2021-12-16 13:24:33 +0800

    commit on 'master'

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const TelegramBot = require('node-telegram-bot-api');
+const token = '5063248322:AAElwf72Cu8wNfYZDho79g2QaHPnlC8xPKw';
+console.log('hello world');
+const bot = new TelegramBot(token, {polling: true});
+bot.onText(/\/start/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Hey there! It's good to see you :)");
+    
+    });
+    
+bot.on('message', (msg) => {
+
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"What album are you vibing with now?");
+    }
+    
+    });
+    
+    
